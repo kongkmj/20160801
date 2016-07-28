84a5b015efa897426117d6cfecfbd0d5095d1add
kongkmj <asd789zx@naver.com>
2016-07-28 16:20:50 +0900

    20160728

---
+++ model/rules/rule001.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var rule001 ={
+  rule001x:{type:String},
+  rule001y:{type:String},
+  range001:{type:String},
+  createdAt:{type:Date,default:Date.now},
+};
+
+module.exports =rule001;
